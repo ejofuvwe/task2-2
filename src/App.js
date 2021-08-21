df24abba6df4b9447a4da07e126cb33386d369e8
ejofuvwe <ejofuvwe@gmail.com>
2021-08-21 23:38:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>EJOMAFUVWE ONOAKPOGBOBOR</h1>
+     
+           <li>address</li>
+           <li>mobile number</li>
+           <li>email</li>
+           <h2>PROFILE</h2>
+             <p>Back-End web developer </p>
+             <h1>Skills</h1>
+             <li>Nodejs</li>
+             <li>express</li>
+             <li>mongodb</li>
+             <h4>zuri.team</h4>
+     <Header />
+    </div>
+    
+  );
+}
+
+export default App;
+
